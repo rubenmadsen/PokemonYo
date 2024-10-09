efe2cea98731d1cf8fdb5918a2468ddcc9ed6b9f
Ruben Madsen <ruben@rubenmadsen.com>
2024-10-09 15:15:31 +0200

    slask

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3 -m venv .venv
 .venv/bin/pip install -r requirements.txt
-python3 generate_raw_models.py
+.venv/python3 generate_raw_models.py
 source update.sh
 .venv/bin/python3 visualizer.py
