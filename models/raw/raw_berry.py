394bff49f1e2d8944a5f8d11f3fa9c4d862324e0
Ruben Madsen <ruben@rubenmadsen.com>
2024-10-09 14:24:23 +0200

    fixes

--- models/raw/raw_berry.py
+++ models/raw/raw_berry.py
@@ -2,13 +2,38 @@ from pandas import DataFrame
 import requests
 import sys
 import os
-
-pool_path = os.path.abspath(os.path.join(__file__, "../../../templates"))
-sys.path.insert(0, pool_path)
-import pool
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import shutil
-
 limit = 1000
+poolsize = 30
+
+def fetch_data_from_endpoints(endpoints, max_workers=poolsize):
+    responses = []
+
+    def fetch(url):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching {url}: {e}")
+            return None
+
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        future_to_url = {executor.submit(fetch, url): url for url in endpoints}
+
+        for future in as_completed(future_to_url):
+            url = future_to_url[future]
+            data = future.result()
+            if data is not None:
+                data["url"] = url
+                responses.append(data)
+                # Optional: Print progress or handle data
+                # print(f"Fetched data from {url}")
+            else:
+                print(f"Failed to fetch data from {url}")
+
+    return responses
 
 
 def model(dbt, sesssion):
@@ -24,7 +49,7 @@ def model(dbt, sesssion):
     # for f in results:
     #     json_obj.append(requests.get(f["url"]).json())
     urls = [obj['url'] for obj in results]
-    json_obj = pool.fetch_data_from_endpoints(urls)
+    json_obj = fetch_data_from_endpoints(urls)
     df = DataFrame(json_obj)
     #shutil.copy('storage/database.db', 'storage/database_reading.db')
     return df
